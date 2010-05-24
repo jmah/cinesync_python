96b64dc65ebb01bc8d2fad8ff3a1a1034a728132
Jonathon Mah <jmah@cinesync.com>
2010-05-24 18:24:32 +0930

    Added MockEnvironment with statement for testing
    This lets us mock out stdin and sys.argv in a nice way.

--- test/event_handler_test.py
+++ test/event_handler_test.py
@@ -9,7 +9,26 @@ sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..', 'l
 import cinesync
 
 
+class MockEnvironment:
+    def __init__(self, argv, stdin):
+        self.mock_argv = argv
+        self.mock_stdin = stdin
+
+    def __enter__(self):
+        self.save_argv = sys.argv
+        self.save_stdin = sys.stdin
+        sys.argv = self.mock_argv
+        sys.stdin = self.mock_stdin
+
+    def __exit__(self, type, value, traceback):
+        sys.stdin = self.save_stdin
+        sys.argv = self.save_argv
+
 class EventHandlerTest(unittest.TestCase):
+    def setUp(self):
+        self.online_args = ['myscript.py', '--key=ASDF0000', '--save-format=JPEG', '--save-dir=/tmp/cinesync']
+        self.offline_args = ['myscript.py', '--key=_OFFLINE_', '--save-format=JPEG', '--save-dir=/tmp/cinesync']
+
     def test_loading_session_from_stdin(self):
         handler_run = [False]
         def my_handler(evt):
@@ -22,8 +41,6 @@ class EventHandlerTest(unittest.TestCase):
 
         path = os.path.join('test', 'v3 files', 'v3-basic.csc')
         with open(path) as file:
-            save_stdin = sys.stdin
-            sys.stdin = file
-            cinesync.event_handler(my_handler)
-            sys.stdin = save_stdin
+            with MockEnvironment(self.online_args, file):
+                cinesync.event_handler(my_handler)
         self.assertTrue(handler_run[0])
