6f1a35dc6380d72003eb267e6fd2fce8dedc591b
Jonathon Mah <jmah@cinesync.com>
2010-05-24 20:21:44 +0930

    Test EventHandler URLs

--- test/event_handler_test.py
+++ test/event_handler_test.py
@@ -14,6 +14,7 @@ class EventHandlerTest(unittest.TestCase):
         self.example_path = os.path.join('test', 'v3 files', 'v3-basic.csc')
         self.online_args = ['myscript.py', '--key=ASDF0000', '--save-format=JPEG', '--save-dir=/tmp/cinesync']
         self.offline_args = ['myscript.py', '--key=_OFFLINE_', '--save-format=PNG', '--save-dir=/tmp/cinesync']
+        self.url_args = ['myscript.py', '--key=URL1111', '--save-format=JPEG', '--url=cinesync://script/myscript?q=hi']
 
     def test_loading_session_from_stdin(self):
         handler_run = False
@@ -23,6 +24,7 @@ class EventHandlerTest(unittest.TestCase):
                 self.assertEqual(s.user_data, 'sessionUserData blah bloo blee')
                 self.assertEqual(len(s.media), 3)
                 self.assertEqual(s.notes, "These are my session notes.\nnewline.")
+                self.assertEqual(evt.url, None)
                 self.assertTrue(s.is_valid())
                 handler_run = True
         self.assertTrue(handler_run)
@@ -35,6 +37,7 @@ class EventHandlerTest(unittest.TestCase):
                 self.assertEqual(evt.session_key, 'ASDF0000')
                 self.assertEqual(evt.save_format, 'JPEG')
                 self.assertEqual(evt.save_parent, '/tmp/cinesync')
+                self.assertEqual(evt.url, None)
                 handler_run = True
         self.assertTrue(handler_run)
 
@@ -46,5 +49,14 @@ class EventHandlerTest(unittest.TestCase):
                 self.assertEqual(evt.session_key, None)
                 self.assertEqual(evt.save_format, 'PNG')
                 self.assertEqual(evt.save_parent, '/tmp/cinesync')
+                self.assertEqual(evt.url, None)
                 handler_run = True
         self.assertTrue(handler_run)
+
+    def test_url_args(self):
+        with open(self.example_path) as file:
+            with cinesync.EventHandler(self.url_args, file) as evt:
+                self.assertFalse(evt.is_offline())
+                self.assertEqual(evt.session_key, 'URL1111')
+                self.assertEqual(evt.save_format, 'JPEG')
+                self.assertEqual(evt.url, 'cinesync://script/myscript?q=hi')
