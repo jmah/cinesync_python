930ab7e243ac6a3e4ba20aff56c9f8b674c1f4fe
Jonathon Mah <jmah@cinesync.com>
2010-05-24 20:55:42 +0930

    Commented Start Session example

--- examples/Start Session.py
+++ examples/Start Session.py
@@ -5,14 +5,17 @@ sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
 import cinesync
 
-
 if len(sys.argv) == 1:
     print >>sys.stderr, 'Usage: %s <file.mov> ...' % sys.argv[0]
 
+# Create the session and add media from command-line arguments
 session = cinesync.Session()
 session.media = [cinesync.MediaFile(path) for path in sys.argv[1:]]
+
+# Write session to temporary file
 f = tempfile.NamedTemporaryFile(suffix='.csc', delete=False)
 f.write(session.to_xml())
 f.close()
 
+# Ask cineSync to add the session to its current state
 cinesync.open_url('cinesync://file/merge?path=%s' % urllib.quote_plus(f.name))
