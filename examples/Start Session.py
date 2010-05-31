23b1969214803441ede6687ed329024f4064dd31
Jonathon Mah <jmah@cinesync.com>
2010-06-01 00:04:58 +0930

    Changed Start Session example to use new commands

--- examples/Start Session.py
+++ examples/Start Session.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python2.6
 
-import sys, os, urllib, tempfile
+import sys, os
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
 import cinesync
@@ -13,10 +13,5 @@ if len(sys.argv) == 1:
 session = cinesync.Session()
 session.media = [cinesync.MediaFile(path) for path in sys.argv[1:]]
 
-# Write session to temporary file
-f = tempfile.NamedTemporaryFile(suffix='.csc', delete=False)
-f.write(session.to_xml())
-f.close()
-
 # Ask cineSync to add the session to its current state
-cinesync.open_url('cinesync://file/merge?path=%s' % urllib.quote_plus(f.name))
+cinesync.commands.do_open_session(session)
